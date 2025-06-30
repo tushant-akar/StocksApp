6a39b23a2d37460ab27daf368dce2be0d56f9a8e
Tushant Akar <106074556+tushant-akar@users.noreply.github.com>
2025-06-30 22:38:29 +0530

    Update Constants.kt

--- app/src/main/java/com/tushant/stocksapp/utils/Constants.kt
+++ app/src/main/java/com/tushant/stocksapp/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.tushant.stocksapp.utils
 
 object Constants {
     const val BASE_URL = "https://www.alphavantage.co/"
-    const val API_KEY = "P0995RK9B359YDL7"
+    const val API_KEY = "demo"
 
     const val CACHE_SIZE = 10 * 1024 * 1024L
     const val CACHE_MAX_AGE = 30
